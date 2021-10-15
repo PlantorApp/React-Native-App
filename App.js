1b3795962e1c227a42bc2293f7f59161f6e039a7
skrispy <skrispyb@gmail.com>
2021-10-15 14:47:30 -0700

    Bot nav (#9)
    * added navigation screens
    
    * Updates folder structure
    
    * Installs react-navigation/native lib and adds 4 bottom navigation buttons
    
    * Plant suggestions pages created but backend not working

--- App.js
+++ App.js
@@ -1,33 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './app/screens/HomeScreen';
-import SavedScreen from './app/screens/SavedScreen';
-import NotificationsScreen from './app/screens/NotificationsScreen';
-import ProfileScreen from './app/screens/ProfileScreen';
+import Nav from './app/components/nav/Nav';
 
-const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Saved" component={SavedScreen} />
-        <Tab.Screen name="Notifications" component={NotificationsScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Nav />
+      </NavigationContainer>
+      <StatusBar style='dark' />
+    </NativeBaseProvider>
+);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
