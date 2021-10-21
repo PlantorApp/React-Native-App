12adae91ad4d7fc5821bd2f5d3aea21ed043570b
Kamaljitwq <kkaur111@mylangara.ca>
2021-10-21 16:24:53 -0700

    notification code

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider } from 'native-base';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Nav from './app/components/nav/Nav';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 
 export default function App() {
-
+  useEffect(()=>{
+    registerForPushNotification().then(token=>console.log(token)).catch(err => console.log(err))
+    
+    }, [])
+    
+      async function registerForPushNotification(){
+      const {status} = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      if (status!='granted'){
+        const {status} = await Permissions.askAsync(Permissions.NOTIFICATIONS)
+    
+      }
+      if (status !='granted'){
+        alert('Fail to get the push token');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      return token
+    
+      }
 
   return (
     <NativeBaseProvider>
