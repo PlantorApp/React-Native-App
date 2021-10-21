963f03be13c87dbe09f6e29b9da20b4a68eb4d20
jaspreet <jsingh150@mylangara.ca>
2021-10-21 15:18:34 -0700

    Saved Screen

--- app/screens/SavedScreen.js
+++ app/screens/SavedScreen.js
@@ -1,26 +1,166 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React,{useState} from "react";
+import { View, Text, Button, StyleSheet,Pressable,
+    Box,
+    FlatList,
+    Heading,
+    Avatar,
+    HStack,
+    VStack,
+    Spacer,
+    Center, } from 'native-base';
 
 const SavedScreen = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-            <Text>Saved</Text>
 
-            <Button 
-                title="Click here"
-                onPress={() => alert('Button Clicked!')}
-            />
-        </View>
+    const [showModal, setShowModal] = useState(false)
+
+    const data = [
+        {
+          id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+          fullName: "Aafreen Khan",
+          timeStamp: "12:47 PM",
+          recentText: "Good Day!",
+          avatarUrl:
+            "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+          fullName: "Sujitha Mathur",
+          timeStamp: "11:11 PM",
+          recentText: "Cheer up, there!",
+          avatarUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU",
+        },
+        {
+          id: "58694a0f-3da1-471f-bd96-145571e29d72",
+          fullName: "Anci Barroco",
+          timeStamp: "6:22 PM",
+          recentText: "Good Day!",
+          avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg",
+        },
+        {
+          id: "68694a0f-3da1-431f-bd56-142371e29d72",
+          fullName: "Aniket Kumar",
+          timeStamp: "8:56 PM",
+          recentText: "All the best",
+          avatarUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU",
+        },
+        {
+          id: "28694a0f-3da1-471f-bd96-142456e29d72",
+          fullName: "Kiara",
+          timeStamp: "12:47 PM",
+          recentText: "I will call today.",
+          avatarUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
+        },
+      ]
+
+
+      const handlePress = (option) => {
+          console.log('Pressed')
+          setShowModal(option)
+      }
+
+    return (
+        <Box w={{ base: "100%", md: "25%",}}>
+            <Heading fontSize="xl" p="4" pb="3">
+                Inbox
+            </Heading>
+            <FlatList
+                data={data}
+                renderItem={({ item }) => (
+                    <>
+                        <Pressable
+                        onPress={() => {
+                            handlePress(true);
+                        }}>
+                            <Box
+                                borderColor="coolGray.200"
+                                borderWidth="1"
+                                _dark={{
+                                    borderColor: "coolGray.600",
+                                    backgroundColor: "gray.700",
+                                }}
+
+                                _web={{
+                                    shadow: 2,
+                                    borderWidth: 0,
+                                }}
+
+                                _light={{
+                                    backgroundColor: "gray.50",
+                                }}
+                                
+                                my="2"
+                                mx="3"
+                                pl="4"
+                                pr="5"
+                                py="2"
+
+                                
+                            >
+                                <HStack space={2}>
+                                    <Avatar
+                                        w={40}
+                                        h={40}
+                                        source={{
+                                        uri: item.avatarUrl,
+                                        }}
+                                    />
+                                    <VStack justifyContent="space-around">
+                                            <Text _dark={{ color: "warmGray.50",}}
+                                                fontSize="3xl"
+                                            color="coolGray.800"
+                                            bold
+                                            >
+                                            {item.fullName}
+                                            </Text>
+
+                                            <Text
+                                            color="coolGray.600"
+                                            fontSize="xl"
+                                            _dark={{
+                                                color: "warmGray.200",
+                                            }}
+                                            >
+                                            {item.recentText}
+                                            </Text>
+
+                                            <Text
+                                            fontSize="xl"
+                                            _dark={{
+                                            color: "warmGray.50",
+                                            }}
+                                            color="coolGray.800"
+                                            alignSelf="flex-start"
+                                        >
+                                            {item.timeStamp}
+                                        </Text>
+                                    </VStack>
+                                    </HStack>
+                            </Box>
+                            </Pressable>  
+                        
+                    </> 
+                     )}
+                keyExtractor={(item) => item.id}
+      />
+      
+    </Box>
+
+
     );
 };
 
-export default SavedScreen;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#8fcbbc'
-    }
-});
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         backgroundColor: '#8fcbbc'
+//     }
+// });
+
+export default SavedScreen;
