61d30615c54f34c2b16e2b28d008a299b6b575ee
skrispy <skrispyb@gmail.com>
2021-11-18 23:35:41 -0800

    more_styling (#33)
    Co-authored-by: Kamaljitwq <kkaur111@mylangara.ca>

--- app/screens/SavedScreen.js
+++ app/screens/SavedScreen.js
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import { Text, Box, FlatList, Image, Heading, HStack, VStack, } from 'native-base';
 import { useFonts } from 'expo-font';
+import { StyleSheet } from 'react-native';
 
 import { MaterialCommunityIcons,MaterialIcons,FontAwesome5 } from '@expo/vector-icons';
 import EditEnv from "../components/dropDown/EditEnv";
@@ -21,6 +22,7 @@ const SavedScreen = ({navigation}) => {
     const data = [
         {
             "id" : "1",
+            "image": require("../assets/savedpage/illusOutdoorSaved.png"),
             "title" : "Home Balcony",
             "season" : "winter",
             "location" : "Vancouver",
@@ -30,7 +32,9 @@ const SavedScreen = ({navigation}) => {
         },
         {
             "id" : "2",
+            "image": require("../assets/savedpage/illusIndoorSaved.png"),
             "title" : "Home Living Room",
+   
             "season" : "winter",
             "location" : "Vancouver",
             "temperature": "20C",
@@ -39,6 +43,7 @@ const SavedScreen = ({navigation}) => {
         },
         {
             "id" : "3",
+            "image": require("../assets/savedpage/illusIndoorSaved.png"),
             "title" : "Office Desk",
             "season" : "winter",
             "location" : "Vancouver",
@@ -48,6 +53,7 @@ const SavedScreen = ({navigation}) => {
         },
         {
             "id" : "4",
+            "image": require("../assets/savedpage/illusIndoorSaved.png"),
             "title" : "Dining Room",
             "season" : "winter",
             "location" : "Vancouver",
@@ -57,6 +63,7 @@ const SavedScreen = ({navigation}) => {
         },
         {
             "id" : "5",
+            "image": require("../assets/savedpage/illusIndoorSaved.png"),
             "title" : "Bed Room",
             "season" : "winter",
             "location" : "Vancouver",
@@ -74,9 +81,12 @@ const SavedScreen = ({navigation}) => {
 
     return (
         <Box w={{ base: "100%", md: "25%",}}>
-            <Heading style={{fontFamily: 'DMSerifText', padding: 4, paddingBottom: 3}}>
-                Saved
-            </Heading>
+      <Heading 
+        style={styles.mainTitle} 
+      >
+      Saved
+      </Heading>
+
             
             <FlatList
                 data={data}
@@ -84,33 +94,36 @@ const SavedScreen = ({navigation}) => {
                             <Box my="2" mx="3" pl="4" pr="5" py="2">
                                 <VStack width="100%">
                                     <HStack justifyContent="space-between">
-                                        <Text fontSize="2xl">{item.title}</Text>
+                                        <Text fontSize="2xl" style={styles.titleStyle}>{item.title}</Text>
                                         <EditEnv />
                                     </HStack>
 
                                     <HStack width="100%" space={2} justifyContent="space-between">
-                                        <Image  source ={{
+                                        {/* <Image  source ={{
                                             uri: item.avatarUrl
-                                        }}  w="100px" h="100px" backgroundColor="rgb(200,150,139)" alt={item.title}/>
+                                        }}  w="100px" h="100px"  alt={item.image}    /> */}
+                                        
+                                        <Image source={item.image} />
+                                        
                                         <VStack space={3}>
-                                            <HStack justifyContent="space-around" space={2}>
+                                            <HStack justifyContent="space-around" space={2} alignItems="center" >
                                                 <Text fontSize="lg">
-                                                    <MaterialCommunityIcons name="weather-cloudy" size={24} color="black"/>
+                                                    <MaterialCommunityIcons name="weather-cloudy" size={24} color="#B7A878"/>
                                                     {item.season}
                                                 </Text>
                                                 <Text fontSize="lg">
-                                                    <MaterialIcons name="location-on" size={24} color="black" />
+                                                    <MaterialIcons name="location-on" size={24} color="#B7A878" />
                                                     {item.light}
                                                 </Text>
                                             </HStack>
 
-                                            <HStack justifyContent="space-around"  space={2}>
+                                            <HStack justifyContent="space-around"  space={2} alignItems="center">
                                                 <Text fontSize="lg">
-                                                    <FontAwesome5 name="temperature-high" size={24} color="black" />
+                                                    <FontAwesome5 name="temperature-high" size={24} color="#B7A878" />
                                                     {item.temperature}
                                                 </Text>
                                                 <Text fontSize="lg">
-                                                    <MaterialCommunityIcons name="weather-sunny" size={24} color="black" />
+                                                    <MaterialCommunityIcons name="weather-sunny" size={24} color="#B7A878" />
                                                     {item.location}
                                                 </Text>
                                             </HStack>
@@ -134,13 +147,25 @@ const SavedScreen = ({navigation}) => {
 
 
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//         backgroundColor: '#8fcbbc'
-//     }
-// });
 
 export default SavedScreen;
+
+
+const styles = StyleSheet.create({
+    // container: {
+    //     flex: 1,
+    //     alignItems: 'center',
+    //     justifyContent: 'center',
+    //     backgroundColor: 'pink'
+    // },
+
+mainTitle: {
+    fontFamily: "DMSerifText", padding: 14, paddingBottom: 15, color: "#827344", fontSize: 32
+  },
+
+titleStyle: {
+    color: "#666666",
+    fontSize: 20,
+    fontFamily: "QuickSandBold"
+}
+});
